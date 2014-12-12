c8ceb76b40018d56655f53215754ec4accf64910
Simon Schurter <simon.schurter@customweb.com>
2014-12-12 12:23:46 +0100

    Add specific css class and style attributes to improve extending templates.

--- src/main/java/com/customweb/jtwig/form/tag/AbstractFormTag.java
+++ src/main/java/com/customweb/jtwig/form/tag/AbstractFormTag.java
@@ -2,10 +2,20 @@ package com.customweb.jtwig.form.tag;
 
 import com.customweb.jtwig.lib.attribute.model.AbstractAttributeTag;
 import com.customweb.jtwig.lib.attribute.model.AttributeCollection;
+import com.customweb.jtwig.lib.attribute.model.definition.AttributeDefinitionCollection;
+import com.customweb.jtwig.lib.attribute.model.definition.NamedAttributeDefinition;
 import com.lyncode.jtwig.content.api.Renderable;
 import com.lyncode.jtwig.render.RenderContext;
 
 abstract public class AbstractFormTag<T extends AbstractFormTag<T>> extends AbstractAttributeTag<T> {
+	
+	@Override
+	public AttributeDefinitionCollection getAttributeDefinitions() {
+		AttributeDefinitionCollection attributeDefinitions = super.getAttributeDefinitions();
+		attributeDefinitions.add(new NamedAttributeDefinition("class", false));
+		attributeDefinitions.add(new NamedAttributeDefinition("style", false));
+		return attributeDefinitions;
+	}
 	
 	abstract protected class Compiled extends AbstractAttributeTag<T>.Compiled {
 		protected Compiled(Renderable block, Renderable content, AttributeCollection attributeCollection) {
@@ -16,6 +26,14 @@ abstract public class AbstractFormTag<T extends AbstractFormTag<T>> extends Abst
 	abstract public class Data extends AbstractAttributeTag<T>.Data {
 		protected Data(RenderContext context, AttributeCollection attributeCollection) {
 			super(context, attributeCollection);
+		}
+		
+		public String getCssClass() {
+			return this.getAttributeValue("class", "");
+		}
+		
+		public String getCssStyle() {
+			return this.getAttributeValue("style", "");
 		}
 	}
 
